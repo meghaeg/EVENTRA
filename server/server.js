b23c91cafd792411572547d690b8135bef09e3a8
meghaeg <meghaeaswaramoorthy@gmail.com>
2026-03-22 00:31:15 +0530

    structure fix2

--- server/server.js
+++ server/server.js
@@ -41,9 +41,8 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 app.use('/api/admin', require('./routes/admin'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
