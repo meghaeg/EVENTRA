9c1a9c443de186c8a6efe9fa305bb8ab12eab358
meghaeg <meghaeaswaramoorthy@gmail.com>
2026-03-22 10:41:40 +0530

    Fixed axios issues

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-    baseURL: 'http://localhost:5050/api',
+    baseURL: `${API}/api`,
     headers: {
         'Content-Type': 'application/json'
     }
